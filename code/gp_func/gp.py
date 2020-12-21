d7d893f0f400692c7032ad8272f76ed7ddd543e8
Qasim Razvi <qr202@cantab.ac.uk>
2020-12-21 15:45:27 +0000

    initial testing. TODO add states

--- code/gp_func/gp.py
+++ code/gp_func/gp.py
@@ -1,9 +1,10 @@
-import sqlite3
 from transitions import Machine
 from loginsystem import database as db
+import cli_ui as ui
 
 
 class Gp:
+
 
     def __init__(self, user_id):
         # Create object from userId object from DB
@@ -15,6 +16,21 @@ class Gp:
         result = self.db.fetch_data(details_query)
         self.firstname, self.lastname = result[0][0].capitalize(), result[0][1].capitalize()
 
+        # define states
+        self.states = [
+            "main_options",  # homepage, initial options
+            "confirm_appts",  # page to confirm appts
+            "calendar_options"  # initial manage cal options
+        ]
+
+        self.flow = Machine(model=self, states=self.states, initial="main_options")
+
     def print_welcome(self):
         print("Hi Dr", self.firstname, self.lastname)
+
+
+    def main_options(self):
+        selected = ui.ask_choice("choose an option", choices=self.states, sort=False)
+        print(selected)
+
 
