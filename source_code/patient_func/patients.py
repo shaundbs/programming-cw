8307f0397b27f3d676248c33adddbc56c020d57c
Yang Zou <dcwx1016@gmail.com>
2021-01-03 21:57:57 +0000

    fix back when view unconfirmed appts

--- source_code/patient_func/patients.py
+++ source_code/patient_func/patients.py
@@ -138,21 +138,20 @@ class Patient:
                     option = list.index(apt, option)
                     if option == 0:
                         self.cancel_appointment(appointmentData[0])
-                        continue
-
-                elif appointmentData[-2] == 0:
-                    print("\nThis appointment is rejected.\n")
-
-                elif appointmentData[-1] == 0:
-                    print("\nThis appointment is confirmed.\n")
-                apt = ["Reschedule this appointment.",
-                       "Cancel this appointment.", "Back."]
-                option = ui.ask_choice("Choose an option", choices=apt, sort=False)
-                option = list.index(apt, option)
-                if option == 0:
-                    self.reschedule_appointment(appointmentData[0])
-                elif option == 1:
-                    self.cancel_appointment(appointmentData[0])
+                    continue
+                elif appointmentData[-2] == 0 or appointmentData[-1] == 0:
+                    if appointmentData[-2] == 0:
+                        print("\nThis appointment is rejected.\n")
+                    elif appointmentData[-1] == 0:
+                        print("\nThis appointment is confirmed.\n")
+                    apt = ["Reschedule this appointment.",
+                           "Cancel this appointment.", "Back."]
+                    option = ui.ask_choice("Choose an option", choices=apt, sort=False)
+                    option = list.index(apt, option)
+                    if option == 0:
+                        self.reschedule_appointment(appointmentData[0])
+                    elif option == 1:
+                        self.cancel_appointment(appointmentData[0])
             else:
                 break
 
