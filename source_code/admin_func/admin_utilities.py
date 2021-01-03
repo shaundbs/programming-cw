d8102c2f3e9bdd9b8abec73fc5118bd74032c31e
Erfan <eric_zafarani@yahoo.co.uk>
2021-01-03 16:56:47 +0200

    added utilities and made some changes to admin file imports

---
+++ source_code/admin_func/admin_utilities.py
@@ -0,0 +1,18 @@
+import logging
+import cli_ui as ui
+from pick import pick
+
+def user_select(prompt: str, choices: list):
+    selected = None
+    while selected is None:
+        try:  # if terminal can support, then cursor selection.
+            choice, index = pick(choices, prompt)
+            selected = choice
+        except Exception as err:
+            # log error
+            logging.info("Exception occurred while trying to use cursors for user input.")
+            try:
+                selected = ui.ask_choice(prompt, choices=choices, sort=False)
+            except AttributeError:
+                print("Please choose a valid option.")
+    return selected
