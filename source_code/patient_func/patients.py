cb3135068f218aa5057fea4bf67c186c426957fb
Shaun <zcfabse@ucl.ac.uk>
2021-01-04 17:15:17 +0000

    relative path for presc files

--- source_code/patient_func/patients.py
+++ source_code/patient_func/patients.py
@@ -387,7 +387,7 @@ class Patient:
         save_path = 'C:/Users/shaun/OneDrive/Documents/MSc Comp Sci/programming-cw/Prescriptions/'
         if presc_opts in [0, 1, 2]:
             if presc_opts == 0:
-                with open(os.path.join(save_path) + 'myprescription.csv', 'w', newline='') as f:
+                with open(os.path.join(save_path) + '../../Prescriptions/myprescription.txt', 'w', newline='') as f:
                     thewriter = csv.writer(f)
                     thewriter.writerow(index_8)
                     for i in output:
@@ -396,7 +396,7 @@ class Patient:
                     print("Your prescription has been saved successfully")
             elif presc_opts == 1:
                 presc_number = 1
-                with open(os.path.join(save_path) + 'myprescription.txt', 'w', newline='') as f:
+                with open('../../Prescriptions/myprescription.txt', 'w', newline='') as f:
                     f.write("Your prescriptions are as follows:\n"
                             "\n")
                     f.write(str(index_8))
