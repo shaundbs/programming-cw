c500047ca8a4d1dcddb33783cf9274b7c636f1ee
GIF1997 <72348648+GIF1997@users.noreply.github.com>
2021-01-06 15:28:22 +0000

    Bug fixes and added approve all/delete all functionality

--- admin_func/confirmPatient.py
+++ admin_func/confirmPatient.py
@@ -28,7 +28,7 @@ def confirm_patient():
         print("Unable to connect to database!")
 
     # Query database for patients in need of validation
-    query = "SELECT userID, firstName, lastName, email, signUpDate FROM Users WHERE is_registered = 0 and accountType = 'patient'"
+    query = "SELECT userID, firstName, lastName, email, signUpDate FROM Users WHERE is_registered = 0 and accountType = 'patient' ORDER BY userID DESC"
     cursor.execute(query)
     records = cursor.fetchall()
 
@@ -55,12 +55,56 @@ def confirm_patient():
         user_ids.append(i[0])
         emails.append(i[3])
     if len(validation_required) == 0:
-        print("\nThere are no patients currently requiring validation!")
+        print("\nThere are no patients currently requiring approval!")
 
         no_patients()
 
     connection.close()
 
+
+def approve_all():
+    patients_validated = []
+
+    connection = sqlite3.connect('database/ehealth.db')
+    cursor = connection.cursor()
+
+
+    for i in range(len(validation_required)):
+        cursor = connection.cursor()
+        update_query = "UPDATE Users SET is_registered = ?, is_active = ?  WHERE userId = ?"
+        data = (1, 1, user_ids[i])
+        cursor.execute(update_query, data)
+        connection.commit()
+        patients_validated.append(validation_required[i])
+
+    connection.close()
+    print("All patients successfully approved!\n")
+    sleep(1)
+    clear()
+    print(
+        f"Number of patients approved: {len(patients_validated)}")
+
+
+def delete_all():
+    patients_deleted = []
+
+    connection = sqlite3.connect('database/ehealth.db')
+    cursor = connection.cursor()
+
+
+    for i in range(len(validation_required)):
+        delete_query = "DELETE from Users WHERE userId = ?"
+        delete_entry = user_ids[i]
+        cursor.execute(delete_query, (delete_entry,))
+        connection.commit()
+        patients_deleted.append(validation_required[i])
+
+    connection.close()
+    print("All patients successfully deleted!\n")
+    sleep(1)
+    clear()
+    print(
+        f"Number of patients deleted: {len(patients_deleted)}")
 
 def validate():
     patients_validated = []
@@ -71,15 +115,15 @@ def validate():
 
     # Loop through each patient and give user option to validate, delete or quit the program
     for i in range(len(validation_required)):
-        print("\nPatient Name:", validation_required[i])
-        action1 = input("1.Validate\n2.Delete\n3.Quit\nSelect option: ")
+        print(f"\nPatient ID: {user_ids[i]}\nPatient Name: {fullname_list[i][0]} {fullname_list[i][1]}")
+        action1 = input("Press enter to skip or choose one of the options below:\n1.Approve\n2.Delete\n3.Quit\nSelect option: ")
         if action1 == "1":
             cursor = connection.cursor()
             update_query = "UPDATE Users SET is_registered = ?, is_active = ?  WHERE userId = ?"
             data = (1, 1, user_ids[i])
             cursor.execute(update_query, data)
             connection.commit()
-            print("Patient successfully validated!\n")
+            print("Patient successfully approved!\n")
             patients_validated.append(validation_required[i])
 
             # Send user email confirming successful validation
@@ -113,7 +157,7 @@ def validate():
     sleep(1)
     clear()
     print(
-        f"Number of patients validated: {len(patients_validated)}\nNumber of patients deleted: {len(patients_deleted)}")
+        f"Number of patients approved: {len(patients_validated)}\nNumber of patients deleted: {len(patients_deleted)}")
 
 
 def no_patients():
